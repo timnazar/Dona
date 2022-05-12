daae918528de2e1ea14e00911a1d196a9baf5ea4
Tim Nazar <timnazar@live.com>
2022-05-12 10:13:00 +0500

    12/05 New test targets

--- Package.swift
+++ Package.swift
@@ -13,6 +13,9 @@ let package = Package(
         .library(
             name: "KintsugiKit",
             targets: ["KintsugiKit"]),
+        .library(
+            name: "KintsugiKitTextField",
+            targets: ["KKTextField"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KintsugiKit",
+            dependencies: []),
+        .target(
+            name: "KKTextField",
             dependencies: []),
         .testTarget(
             name: "KintsugiKitTests",
