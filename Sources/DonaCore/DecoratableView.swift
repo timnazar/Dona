f3424af89d12ce8d567abddde3898a43ea496c5e
Tim Nazar <timnazar@live.com>
2023-05-14 13:18:38 +0500

    Change access level for init

--- Sources/DonaCore/DecoratableView.swift
+++ Sources/DonaCore/DecoratableView.swift
@@ -11,7 +11,7 @@ public protocol DonaDecoratableView: UIView {}
 
 extension DonaDecoratableView {
     /// Current decorators applied to the view.
-    var currentDecorators: [DonaViewDecorator<Self>] {
+    public var currentDecorators: [DonaViewDecorator<Self>] {
         get {
             return objc_getAssociatedObject(self, "currentDecorators") as? [DonaViewDecorator<Self>] ?? []
         }
@@ -21,26 +21,26 @@ extension DonaDecoratableView {
     }
 
     /// An initializer that takes a ViewDecorator and applies it to the view.
-    init(decorator: DonaViewDecorator<Self>) {
+    public init(decorator: DonaViewDecorator<Self>) {
         self.init(frame: .zero)
         decorate(with: decorator)
     }
 
     /// Applies a ViewDecorator to the view and returns the view for chaining.
     @discardableResult
-    func decorated(with decorator: DonaViewDecorator<Self>) -> Self {
+    public func decorated(with decorator: DonaViewDecorator<Self>) -> Self {
         decorate(with: decorator)
         return self
     }
 
     /// Applies a ViewDecorator to the view and adds it to the current decorators array.
-    func decorate(with decorator: DonaViewDecorator<Self>) {
+    public func decorate(with decorator: DonaViewDecorator<Self>) {
         decorator.decorate(self)
         currentDecorators.append(decorator)
     }
 
     /// Reapplies all the current decorators to the view.
-    func redecorate() {
+    public func redecorate() {
         currentDecorators.forEach {
             $0.decorate(self)
         }
