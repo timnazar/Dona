db878251294dad70ab1a7b7cd0b952c98da8a950
Tim Nazar <timnazar@live.com>
2023-05-14 19:35:04 +0500

    Add more common decorators

--- Sources/DonaCore/ViewDecorator+Common.swift
+++ Sources/DonaCore/ViewDecorator+Common.swift
@@ -41,4 +41,62 @@ public extension DonaViewDecorator {
             $0.tintColor = color
         }
     }
+
+
+    static func contentMode(_ contentMode: UIView.ContentMode) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> {
+            $0.contentMode = contentMode
+        }
+    }
+
+    static func textAlignment(_ textAlignment: NSTextAlignment) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> { view in
+            if let label = view as? UILabel {
+                label.textAlignment = textAlignment
+            } else if let textField = view as? UITextField {
+                textField.textAlignment = textAlignment
+            } else if let button = view as? UIButton {
+                button.titleLabel?.textAlignment = textAlignment
+            }
+        }
+    }
+
+    // MARK: - UILabel's Decorators.
+    static func numberOfLines(_ lines: Int) -> DonaViewDecorator<UILabel> {
+        DonaViewDecorator<UILabel> {
+            $0.numberOfLines = lines
+        }
+    }
+
+    static func font(_ font: UIFont) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> { view in
+            if let label = view as? UILabel {
+                label.font = font
+            } else if let textField = view as? UITextField {
+                textField.font = font
+            } else {
+                if let button = view as? UIButton {
+                    button.titleLabel?.font = font
+                }
+            }
+        }
+    }
+
+    static func textColor(_ color: UIColor) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> { view in
+            if let label = view as? UILabel {
+                label.textColor = color
+            }
+        }
+    }
+
+    // MARK: - UIButton
+
+    static func systemImage(_ imageName: String) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> {
+            if let button = $0 as? UIButton {
+                button.setImage(UIImage(systemName: imageName), for: .normal)
+            }
+        }
+    }
 }
