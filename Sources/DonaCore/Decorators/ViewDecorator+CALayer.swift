b3072fe188d79e7f0f67ac3959b7673482249a07
Tim Nazar <timnazar@live.com>
2023-06-23 10:02:48 +0500

    Add decorators

---
+++ Sources/DonaCore/Decorators/ViewDecorator+CALayer.swift
@@ -0,0 +1,30 @@
+//
+//  ViewDecorator+CALayer.swift
+//  
+//
+//  Created by Tim Nazar on 6/23/23.
+//
+
+import UIKit
+
+public extension DonaViewDecorator {
+    static func cornerCurve(_ cornerCurve: CALayerCornerCurve ) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> {
+            if #available(iOS 13.0, *) {
+                $0.layer.cornerCurve = cornerCurve
+            }
+        }
+    }
+
+    static func cornerRadius(_ cornerRadius: CGFloat) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> {
+            $0.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    static func borderWidth(_ borderWidth: CGFloat) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> {
+            $0.layer.borderWidth = borderWidth
+        }
+    }
+}
