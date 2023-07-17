da7396b6f7cc18f9510faf4e88f7fdf613eefbff
Tim Nazar <timnazar@live.com>
2023-07-17 12:49:49 +0500

    Add contentInset decorator

---
+++ Sources/DonaCore/Decorators/ViewDecorator+Containers.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tim Nazar on 7/17/23.
+//
+
+import UIKit
+
+extension DonaViewDecorator {
+    static func contentInset(_ insets: UIEdgeInsets) -> DonaViewDecorator<View> {
+        DonaViewDecorator<View> { view in
+            if let stackView = view as? UIStackView {
+                stackView.isLayoutMarginsRelativeArrangement = true
+                stackView.layoutMargins = insets
+            } else if let tableView = view as? UITableView {
+                tableView.contentInset = insets
+            } else if let collectionView = view as? UICollectionView {
+                collectionView.contentInset = insets
+            }
+            // TODO: handle insets for UITextField and UIButton
+        }
+    }
+}
