a20c4631d76762f7fc953c76e0d881b78d691b59
Behruz Numonov <bxncorp@gmail.com>
2024-03-13 10:40:15 +0500

    Add delegate, delaysContentTouches, keyboardDismissMode for UIScrollView

--- Sources/DonaCore/Decorators/ViewDecorator+UIScrollView.swift
+++ Sources/DonaCore/Decorators/ViewDecorator+UIScrollView.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 public extension DonaViewDecorator {
+    static func delegate(_ delegate: UIScrollViewDelegate) -> DonaViewDecorator<UIScrollView> {
+        DonaViewDecorator<UIScrollView> {
+            $0.delegate = delegate
+        }
+    }
+
     static func showsVerticalScrollIndicator(_ showsVerticalScrollIndicator: Bool) -> DonaViewDecorator<UIScrollView> {
         DonaViewDecorator<UIScrollView> {
             $0.showsVerticalScrollIndicator = showsVerticalScrollIndicator
@@ -23,6 +29,18 @@ public extension DonaViewDecorator {
     static func contentInsetAdjustmentBehavior(_ contentInsetAdjustmentBehavior: UIScrollView.ContentInsetAdjustmentBehavior) -> DonaViewDecorator<UIScrollView> {
         DonaViewDecorator<UIScrollView> {
             $0.contentInsetAdjustmentBehavior = contentInsetAdjustmentBehavior
+        }
+    }
+
+    static func delaysContentTouches(_ delaysContentTouches: Bool) -> DonaViewDecorator<UIScrollView> {
+        DonaViewDecorator<UIScrollView> {
+            $0.delaysContentTouches = delaysContentTouches
+        }
+    }
+
+    static func keyboardDismissMode(_ keyboardDismissMode: UIScrollView.KeyboardDismissMode) -> DonaViewDecorator<UIScrollView> {
+        DonaViewDecorator<UIScrollView> {
+            $0.keyboardDismissMode = keyboardDismissMode
         }
     }
 }
